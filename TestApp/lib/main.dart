a91b6e4402f84d5c00b6baf2c6ceb4cd219174ff
Ivy Ti <chivy1221@gmail.com>
2020-11-02 00:33:05 +0200

    loop with template

--- TestApp/lib/main.dart
+++ TestApp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() {
   runApp(MaterialApp(home: QuoteList()));
@@ -10,11 +11,41 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
-  List<String> quotes = [
-    'The only joy in the world is to begin.',
-    'What you do not want done to yourself, do not do to others.',
-    'Happiness is not something ready-made.'
+  List<Quote> quotes = [
+    Quote(text: 'The only joy in the world is to begin.', author: 'Beyonce'),
+    Quote(
+        text: 'What you do not want done to yourself, do not do to others.',
+        author: "Lupita Nyongo"),
+    Quote(
+        text: 'Happiness is not something ready-made.',
+        author: 'Michelle Obama')
   ];
+
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(fontSize: 18.0, color: Colors.grey[800]),
+            ),
+            SizedBox(
+              height: 6.0,
+            ),
+            Text(
+              quote.author,
+              style: TextStyle(fontSize: 14.0, color: Colors.grey[800]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +56,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((e) => Text(e)).toList(),
+        children: quotes.map((e) => quoteTemplate(e)).toList(),
       ),
     );
   }
