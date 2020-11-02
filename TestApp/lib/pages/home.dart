b0f9d73476699db2b53a76be7a74a58134b6c926
Ivy Ti <chivy1221@gmail.com>
2020-11-02 21:26:32 +0200

    Tenary operators

--- TestApp/lib/pages/home.dart
+++ TestApp/lib/pages/home.dart
@@ -12,39 +12,61 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     data = ModalRoute.of(context).settings.arguments;
     print(data);
+
+    String bgImage = data['dayTime'] ? 'img-1.jpg' : 'img-3.jpg';
+    Color bgCol = data['dayTime'] ? Colors.pink[100] : Colors.redAccent[400];
+
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-        child: SafeArea(
-            child: Column(
-          children: [
-            FlatButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text('Edit Location')),
-            SizedBox(
-              height: 20.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  data['location'],
-                  style: TextStyle(fontSize: 20.0, letterSpacing: 2.0),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              data['time'],
-              style: TextStyle(fontSize: 66),
-            )
-          ],
-        )),
+      backgroundColor: bgCol,
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/$bgImage'), fit: BoxFit.cover)),
+          child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+              child: Column(
+                children: [
+                  FlatButton.icon(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/location');
+                    },
+                    icon: Icon(
+                      Icons.edit_location,
+                      color: Colors.red[900],
+                    ),
+                    label: Text(
+                      'Edit Location',
+                      style: TextStyle(color: Colors.red[900]),
+                    ),
+                    color: Colors.red[50],
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        data['location'],
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            letterSpacing: 2.0,
+                            color: Colors.deepOrangeAccent),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    data['time'],
+                    style:
+                        TextStyle(fontSize: 66, color: Colors.deepOrangeAccent),
+                  )
+                ],
+              )),
+        ),
       ),
     );
   }
--- TestApp/lib/services/world_time.dart
+++ TestApp/lib/services/world_time.dart
@@ -7,6 +7,7 @@ class WorldTime {
   String time;
   String flag;
   String url;
+  bool isDayTime;
 
   WorldTime({this.location, this.flag, this.url});
 
@@ -22,6 +23,7 @@ class WorldTime {
       DateTime now = DateTime.parse(dateTime);
       now = now.add(Duration(hours: int.parse(offset)));
 
+      isDayTime = now.hour > 6 && now.hour < 14 ? true : false;
       time = DateFormat.jm().format(now);
     } catch (e) {
       print("caught error: $e");
