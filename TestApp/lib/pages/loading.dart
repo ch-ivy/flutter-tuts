609761099d99a27752fdf825cd4236ceba057b99
Ivy Ti <chivy1221@gmail.com>
2020-11-02 19:50:27 +0200

    Error handling

--- TestApp/lib/pages/loading.dart
+++ TestApp/lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:TestApp/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -8,29 +7,31 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getTime() async {
-    Response response =
-        await get("http://worldtimeapi.org/api/timezone/Europe/London");
-    Map data = jsonDecode(response.body);
+  String time = 'loading';
 
-    String dateTime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-
-    DateTime now = DateTime.parse(dateTime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
+  void setUpWorldTime() async {
+    WorldTime instance =
+        WorldTime(location: 'Cyprus', flag: '', url: 'Europe/Cyprus');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    getTime();
+    setUpWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading Screen'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
