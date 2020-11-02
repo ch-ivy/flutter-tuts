86a53a13d78b3af8d910820490b2d7e1c15de32b
Ivy Ti <chivy1221@gmail.com>
2020-11-02 05:11:44 +0200

    routes

--- TestApp/lib/main.dart
+++ TestApp/lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:TestApp/pages/choose_location.dart';
+import 'package:TestApp/pages/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:TestApp/pages/home.dart';
 
 void main() {
-  runApp(MaterialApp(home: Home()));
+  runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
+  ));
 }
--- TestApp/lib/pages/choose_location.dart
+++ TestApp/lib/pages/choose_location.dart
@@ -9,7 +9,14 @@ class _ChooseLocationState extends State<ChooseLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Choose Location Screen'),
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Text('Edit Location'),
     );
   }
 }
