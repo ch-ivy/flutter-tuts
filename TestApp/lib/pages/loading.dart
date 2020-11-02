9c35d1f5518b0b31c7364c4c0b234ac59173d8e4
Ivy Ti <chivy1221@gmail.com>
2020-11-02 20:18:56 +0200

    loaders and spinners

--- TestApp/lib/pages/loading.dart
+++ TestApp/lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:TestApp/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -29,9 +30,11 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text('Loading'),
+      body: Center(
+        child: SpinKitChasingDots(
+          color: Colors.blueGrey[300],
+          size: 100.0,
+        ),
       ),
     );
   }
